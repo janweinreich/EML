37518b5bdadbc73838463b04fff3b7a58d31a096
janweinreich <janweinreich286@gmail.com>
2022-12-05 09:05:40 +0100

    update

---
+++ NN_CRYPTO/crypto_net.py
@@ -0,0 +1,259 @@
+import torch
+import torch.nn as nn 
+import torch.nn as nn
+import torch.nn.functional as F
+import crypten
+import crypten.mpc as mpc
+import crypten.communicator as comm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import time
+import random
+from tqdm import tqdm
+random.seed(1337)
+np.random.seed(1337)
+
+
+class AliceNet(nn.Module):
+    """
+    Class for an encrypted neural network
+    """
+
+    def __init__(self, in_dim=None, n_neuro=None, lr=None, num_epochs=None,X_test=None, y_test=None, name=None, verbose=None):
+        """
+        in_dim: input dimension
+        n_neuro: number of neurons in each layer
+        lr: learning rate
+        num_epochs: number of epochs
+        X_test: test data
+        y_test: test labels
+        name: name of the model
+        verbose: print loss every 100 epochs
+        """
+        super(AliceNet, self).__init__()
+
+        self.in_dim      = in_dim or 276
+        self.n_neuo      = n_neuro or 300
+        self.lr          = lr or 0.0003
+        self.num_epochs  = num_epochs or 10
+        self.name        = name or "ALICE_NET"
+        self.y_test      = y_test #or None
+        self.X_test      = X_test
+        self.loss        = nn.MSELoss()
+        self.verbose     = verbose or False
+        
+        self.fc1 = nn.Linear(self.in_dim, 2*self.n_neuo)
+        self.fc2 = nn.Linear(2*self.n_neuo, self.n_neuo)
+        self.fc3 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc4 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc5 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc6 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc7 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc8 = nn.Linear(self.n_neuo, self.n_neuo)
+        self.fc9 = nn.Linear(self.n_neuo, 1)
+ 
+    def forward(self, x):
+        """
+        forward pass
+        """
+        act = F.relu #selu works much much better
+        out = self.fc1(x)
+        out = act(out)
+        out = self.fc2(out)
+        out = act(out)
+        out = self.fc3(out)
+        out = act(out)
+        out = self.fc4(out)
+        out = act(out)
+        out = self.fc5(out)
+        out = act(out)
+        out = self.fc6(out)
+        out = act(out)
+        out = self.fc7(out)
+        out = act(out)
+        out = self.fc8(out)
+        out = act(out)
+        out = self.fc9(out)
+        return out
+
+
+    def fit(self, X_train, y_train):
+        """
+        fit the model to the data X_train, y_train
+        minibatch training is used to train the model on the encrypted data
+        """
+        optimizer = torch.optim.Adam(self.parameters(), lr =self.lr )
+        batch_size = 256 # or whatever
+
+        for ep in range(self.num_epochs):
+
+            permutation = torch.randperm(X_train.size()[0])
+
+            for i in range(0,X_train.size()[0], batch_size):
+                optimizer.zero_grad()
+
+                indices = permutation[i:i+batch_size]
+                batch_x, batch_y = X_train[indices], y_train[indices]
+                y_pred = model.forward(batch_x)
+            
+                loss= self.loss(y_pred, batch_y)
+
+                loss.backward()
+                optimizer.step()
+            if self.verbose and ep%100==0:
+                print('epoch {}, loss {}'.format(ep, loss.item()))
+
+            valid_loss = self.loss(self.y_test, self.forward(self.X_test))
+            if loss < valid_loss*0.0015:
+                break
+
+
+    def predict(self, X_test):
+        """
+        predict the labels of the test data
+        """
+        return self(X_test)
+
+    def save(self):
+        """
+        save the model
+        """
+        torch.save(self, self.name)
+
+
+class Crypto():
+    """
+    Class for the crypto network,
+    handles the communication between the two parties
+    """
+
+    global ALICE
+    global BOB
+    ALICE, BOB = 0,1
+
+    def __init__(self, model_saved, BOB_DATA):
+        self.model      = torch.load(model_saved)
+        self.bob_data   = BOB_DATA
+        
+    @mpc.run_multiprocess(world_size=2)
+    def encrypt_query_data(self):
+        crypten.save_from_party(self.bob_data,"BOB_data", src=BOB)
+
+    def encrypt_model(self):
+        crypten.init()
+        torch.set_num_threads(1)
+        crypten.common.serial.register_safe_class(AliceNet)
+
+        first_parameter = next(self.model.parameters())
+        input_shape = first_parameter.size()[1]
+
+        dummy_input = torch.empty((1, input_shape))
+        self.private_model = crypten.nn.from_pytorch(self.model, dummy_input)
+        self.private_model.encrypt(src=ALICE)
+
+    @mpc.run_multiprocess(world_size=2)
+    def encrypted_pred(self):
+        self.encrypt_model()
+        self.encrypt_query_data()
+
+        self.query_data =  crypten.load_from_party("BOB_data",  src=BOB)
+        self.private_model.eval()
+        output_enc = self.private_model(self.query_data)
+        output = output_enc.get_plain_text()
+        output = np.array(output.detach().numpy())
+        return output
+
+
+class Scaler():
+    def __init__(self):
+        self.maxval = None
+        self.minval = None
+
+    def fit(self, X):
+        self.maxval  = np.max(X, axis=0)
+        self.minval  = np.min(X, axis=0)
+    
+    
+    def transform(self,X):
+        X_std = (X - self.minval / (self.maxval - self.minval))
+        X_scaled = X_std * (self.maxval- self.minval) + self.minval
+        return X_scaled
+
+
+
+if __name__ == "__main__":
+    fit = False
+
+    if fit:
+
+        new = False
+
+        if new:
+            import get_qm9 
+            #load the QM9 dataset in BoB representation and atomization energies
+            X, y = get_qm9.get_qm9(140000)
+
+            np.savez_compressed("data",X=X, y=y)
+        else:
+            data = np.load("data.npz", allow_pickle=True)
+            X, y = data["X"], data["y"]
+
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+        y_train = y_train+ np.random.normal(0, 1e-4*np.std(y_train), y_train.shape)
+
+        y_train, y_test     = y_train.reshape(-1,1), y_test.reshape(-1,1)
+
+        sc          = MinMaxScaler()
+        sct         = MinMaxScaler()
+        X_train     = sc.fit_transform(X_train)
+        X_test      = sc.transform(X_test)
+        y_train     = sct.fit_transform(y_train)
+        y_test      = sct.transform(y_test)
+
+        print(len(y_train))
+        print(len(y_test))
+
+        X_train     = torch.from_numpy(X_train.astype(np.float32))
+        X_test      = torch.from_numpy(X_test.astype(np.float32))
+        y_train     = torch.from_numpy(y_train.astype(np.float32))
+        y_test      = torch.from_numpy(y_test.astype(np.float32))
+
+
+
+        N = [2**i for i in range(6,17)]
+        N.append(len(y_train))
+        for n in N:
+            model = AliceNet(in_dim=X_train.shape[1], n_neuro=250, num_epochs=8000,y_test=y_test.cuda(), X_test=X_test.cuda(),verbose=True)
+            model.to(torch.device("cuda"))
+            model.fit(X_train[:n].cuda(), y_train[:n].cuda())
+            model.to(torch.device("cpu"))
+            model.save()
+
+
+            crypto_predictions    = []
+            plaintext_predictions = []
+            
+            for i in tqdm(range(len(X_test))       ) :
+                X_curr = X_test[i].reshape((1, X_test.shape[1]))
+                qml_crypto = Crypto("ALICE_NET",X_curr)
+                tic = time.perf_counter()
+                pred_crypto = sct.inverse_transform( qml_crypto.encrypted_pred()[1]).flatten()
+                toc = time.perf_counter()
+                dT   = toc -tic
+
+                pred        = sct.inverse_transform(np.array(model.predict(X_curr).detach()))
+
+                crypto_predictions.append(pred_crypto)
+                plaintext_predictions.append(pred)
+
+            crypto_predictions, plaintext_predictions  = np.array(crypto_predictions),       np.array(plaintext_predictions)
+            crypto_predictions, plaintext_predictions  = np.concatenate(crypto_predictions), np.concatenate(plaintext_predictions).flatten()
+            num_mae     = np.mean(np.abs(crypto_predictions-plaintext_predictions))
+            mae_plain   = np.mean(np.abs(plaintext_predictions -    sct.inverse_transform(y_test).flatten()))
+            mae         = np.mean(np.abs(crypto_predictions    -    sct.inverse_transform(y_test).flatten()))
+            print("#",n ,mae_plain, mae,num_mae)
+            plt.plot(crypto_predictions, sct.inverse_transform(y_test).flatten(), "o")
+
---
+++ NN_CRYPTO/get_qm9.py
@@ -0,0 +1,194 @@
+import numpy as np
+import qml
+from tqdm import tqdm
+import numpy as np
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import pickle as pickle
+import random
+from qml.utils import NUCLEAR_CHARGE
+
+random.seed(1337)
+np.random.seed(1337)
+
+"""
+=========================================================================================================
+  Ele-    ZPVE         U (0 K)      U (298.15 K)    H (298.15 K)    G (298.15 K)     CV
+  ment   Hartree       Hartree        Hartree         Hartree         Hartree        Cal/(Mol Kelvin)
+=========================================================================================================
+   H     0.000000     -0.500273      -0.498857       -0.497912       -0.510927       2.981
+   C     0.000000    -37.846772     -37.845355      -37.844411      -37.861317       2.981
+   N     0.000000    -54.583861     -54.582445      -54.581501      -54.598897       2.981
+   O     0.000000    -75.064579     -75.063163      -75.062219      -75.079532       2.981
+   F     0.000000    -99.718730     -99.717314      -99.716370      -99.733544       2.981
+=========================================================================================================
+"""
+
+
+def get_atomsizes(compounds):
+    charge_to_element = {
+        1: 'H',
+        6: 'C',
+        7: 'N',
+        8: 'O',
+        9: 'F',
+        16: 'S',
+        17: 'Cl',
+        53: 'I',
+        35: 'Br',
+        15: 'P'}
+    asize_dict = {'H': 0, 'C': 0, 'N': 0, 'O': 0, 'F': 0, 'Cl': 0, 'S':0, 'I':0, 'Br':0, 'P':0}
+    for compound in compounds:
+        nc, counts = np.unique(compound.nuclear_charges, return_counts=True)
+        for z, count in zip(nc, counts):
+            if asize_dict[charge_to_element[z]] < count:
+                asize_dict[charge_to_element[z]] = count
+    return asize_dict
+
+
+
+
+    return replist
+
+def atomization_en(EN, ATOMS):
+  import collections, numpy
+  en_H = -0.500273 
+  en_C = -37.846772
+  en_N = -54.583861
+  en_O = -75.064579 
+  en_F = -99.718730
+  COMP =  collections.Counter(ATOMS)
+  
+  ATOMIZATION = (EN - (COMP['H']*en_H + COMP['C']*en_C + COMP['N']*en_N +  COMP['O']*en_O +  COMP['F']*en_F))
+
+  return ATOMIZATION #/Ntot
+  
+def get_atomsizes(compounds):
+    charge_to_element = {
+        1: 'H',
+        6: 'C',
+        7: 'N',
+        8: 'O',
+        9: 'F',
+        16: 'S',
+        17: 'Cl',
+        53: 'I',
+        35: 'Br',
+        15: 'P'}
+    asize_dict = {'H': 0, 'C': 0, 'N': 0, 'O': 0, 'F': 0, 'Cl': 0, 'S':0, 'I':0, 'Br':0, 'P':0}
+    for compound in compounds:
+        nc, counts = np.unique(compound.nuclear_charges, return_counts=True)
+        for z, count in zip(nc, counts):
+            if asize_dict[charge_to_element[z]] < count:
+                asize_dict[charge_to_element[z]] = count
+    return asize_dict
+
+
+def get_bob(mol_data):
+    all_compounds = []
+    for mol in mol_data:
+        cmp = qml.Compound()
+        cmp.coordinates = mol[1]
+        cmp.atomtypes = mol[0]
+        cmp.nuclear_charges = np.array([NUCLEAR_CHARGE[ele] for ele in cmp.atomtypes]).flatten()     
+        all_compounds.append(cmp)
+
+
+    max_atoms = max([z.nuclear_charges.shape[0] for z in all_compounds])
+    asize = get_atomsizes(all_compounds)
+
+    for compound in tqdm(all_compounds, total=len(all_compounds)):
+        compound.generate_bob(size=max_atoms, asize=asize)
+
+    replist = np.array([compound.representation for compound in all_compounds])
+
+    return replist
+
+def read_xyz(path):
+    """
+    Reads the xyz files in the directory on 'path'
+    Input
+    path: the path to the folder to be read
+    
+    Output
+    atoms: list with the characters representing the atoms of a molecule
+    coordinates: list with the cartesian coordinates of each atom
+    smile: list with the SMILE representation of a molecule
+    prop: list with the scalar properties
+    """
+    atoms = []
+    coordinates = []
+
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        n_atoms = int(lines[0])  # the number of atoms
+        smile = lines[n_atoms + 3].split()[0]  # smiles string
+        prop = lines[1].split()[2:]  # scalar properties
+        
+        # to retrieve each atmos and its cartesian coordenates
+        for atom in lines[2:n_atoms + 2]:
+            line = atom.split()
+            # which atom
+            atoms.append(line[0])
+
+            # its coordinate
+            # Some properties have '*^' indicading exponentiation 
+            try:
+                coordinates.append(
+                    (float(line[1]),
+                     float(line[2]),
+                     float(line[3]))
+                    )
+            except:
+                coordinates.append(
+                    (float(line[1].replace('*^', 'e')),
+                     float(line[2].replace('*^', 'e')),
+                     float(line[3].replace('*^', 'e')))
+                    )
+                    
+    return np.array(atoms), np.array(coordinates), smile, prop
+
+def get_qm9(N):
+
+    directory = '/store/jan/datasets/qm9/'
+    len(os.listdir(directory))
+
+    file = os.listdir(directory)[0]
+    with open(directory+file, 'r') as f:
+
+        content = f.readlines()
+
+    all_mol_paths = os.listdir(directory)
+    np.random.shuffle(all_mol_paths)
+    all_mol_paths = all_mol_paths[:N]
+
+    data = []
+    smiles = []
+    properties = []
+    for file in tqdm(all_mol_paths):
+        path = os.path.join(directory, file)
+        atoms, coordinates, smile, prop = read_xyz(path)
+        data.append((atoms, coordinates))
+        smiles.append(smile) 
+
+        ATOMIZATION = atomization_en(float(prop[10]),atoms )
+        prop += [ATOMIZATION]
+        properties.append(prop) # The molecules properties
+
+
+    
+
+
+    properties_names = ['A', 'B', 'C', 'mu', 'alfa', 'homo', 'lumo', 'gap', 'R²', 'zpve', 'U0', 'U', 'H', 'G', 'Cv', 'atomization']
+    df = pd.DataFrame(properties, columns = properties_names)
+    df['smiles'] = smiles
+    print(df.head())
+    y  = df['atomization'].values
+
+
+    data = np.array(data)
+    X= get_bob(data)
+
+    return X, y

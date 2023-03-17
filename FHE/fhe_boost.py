1132d34acfb6137c00c8add0206ca19fa283d07a
janweinreich <janweinreich286@gmail.com>
2023-03-17 13:25:56 +0100

    danesh representation

--- FHE/fhe_boost.py
+++ FHE/fhe_boost.py
@@ -18,6 +18,9 @@ from concrete.ml.sklearn import XGBRegressor as ConcreteXGBRegressor, Ridge
 import random
 import MBDF
 import pdb
+from tempfile import TemporaryDirectory
+from concrete.ml.deployment import FHEModelClient, FHEModelDev, FHEModelServer
+from shutil import copyfile
 np.random.seed(42)
 random.seed(42)
 
@@ -60,7 +63,8 @@ class data_preprocess:
 
     def gen_rep(self):
         mbdf = MBDF.generate_mbdf(self.nuclear_charges,self.coords, n_jobs=-1,normalized = False, progress=False)
-        self.X    = MBDF.generate_DF(mbdf,self.nuclear_charges,  binsize=self.bin_size)
+        pdb.set_trace()
+        self.X = MBDF.generate_DF(mbdf,self.nuclear_charges,  binsize=self.bin_size)
 
     def split_data(self):
         X_train, X_test, y_train, y_test = train_test_split(self.X, self.properties, test_size=0.2, random_state=42)
@@ -116,14 +120,75 @@ class fhe_boost:
         return self.concrete_reg.predict(X_test, execute_in_fhe=execute_in_fhe)
 
 
+class OnDiskNetwork:
+    """Simulate a network on disk."""
+
+    def __init__(self):
+        # Create 3 temporary folder for server, client and dev with tempfile
+        self.server_dir = TemporaryDirectory()  # pylint: disable=consider-using-with
+        self.client_dir = TemporaryDirectory()  # pylint: disable=consider-using-with
+        self.dev_dir = TemporaryDirectory()  # pylint: disable=consider-using-with
+
+    def client_send_evaluation_key_to_server(self, serialized_evaluation_keys):
+        """Send the public key to the server."""
+        with open(self.server_dir.name + "/serialized_evaluation_keys.ekl", "wb") as f:
+            f.write(serialized_evaluation_keys)
+
+    def client_send_input_to_server_for_prediction(self, encrypted_input):
+        """Send the input to the server and execute on the server in FHE."""
+        with open(self.server_dir.name + "/serialized_evaluation_keys.ekl", "rb") as f:
+            serialized_evaluation_keys = f.read()
+        time_begin = time.time()
+        encrypted_prediction = FHEModelServer(self.server_dir.name).run(
+            encrypted_input, serialized_evaluation_keys
+        )
+        time_end = time.time()
+        with open(self.server_dir.name + "/encrypted_prediction.enc", "wb") as f:
+            f.write(encrypted_prediction)
+        return time_end - time_begin
+
+    def dev_send_model_to_server(self):
+        """Send the model to the server."""
+        copyfile(self.dev_dir.name + "/server.zip", self.server_dir.name + "/server.zip")
+
+    def server_send_encrypted_prediction_to_client(self):
+        """Send the encrypted prediction to the client."""
+        with open(self.server_dir.name + "/encrypted_prediction.enc", "rb") as f:
+            encrypted_prediction = f.read()
+        return encrypted_prediction
+
+    def dev_send_clientspecs_and_modelspecs_to_client(self):
+        """Send the clientspecs and evaluation key to the client."""
+        copyfile(self.dev_dir.name + "/client.zip", self.client_dir.name + "/client.zip")
+        copyfile(
+            self.dev_dir.name + "/serialized_processing.json",
+            self.client_dir.name + "/serialized_processing.json",
+        )
+
+    def cleanup(self):
+        """Clean up the temporary folders."""
+        self.server_dir.cleanup()
+        self.client_dir.cleanup()
+        self.dev_dir.cleanup()
+
 
 # main
 if __name__ == "__main__":
+
+    #Development Server
     X_train, X_test, y_train, y_test = data_preprocess().run()
+    #pdb.set_trace()
     fhe_boost = fhe_boost(X_train, y_train)
 
     fhe_boost.cross_validation(N_max=100)
     fhe_boost.quantize_model(N_max=100)
+    print("Model trained and compiled.")
+
+    # Let's instantiate the network
+    network = OnDiskNetwork()
+    fhemodel_dev = FHEModelDev(network.dev_dir.name, fhe_boost.concrete_reg)
+    fhemodel_dev.save()
+    print(os.listdir(network.dev_dir.name))
     pdb.set_trace()
-    y_pred = fhe_boost.predict(X_test, execute_in_fhe=True)
+    #y_pred = fhe_boost.predict(X_test, execute_in_fhe=True)
     
